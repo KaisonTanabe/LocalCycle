84eb7bed75f7c9178291cdeeda170057928c07b7
Macklin Chaffee <macklin@goldenorbventure.com>
2013-04-03 08:57:48 -0600

    Removed old tests

--- test/functional/signatures_needed_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SignaturesNeededTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/plays_sport_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlaysSportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
