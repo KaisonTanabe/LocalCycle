9722ae89c89b5d30abbd326c2a32b1a4ff29053f
Macklin Chaffee <macklin@goldenorbventure.com>
2013-04-03 21:48:47 -0600

    Added public page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,38 @@
 LOCALCYCLE::Application.routes.draw do
 
-#  resources :categories
+  resources :categories
 
-#  resources :agreements
+  resources :agreements
 
-#  resources :buyer_profiles, only: ["new","create"]
+  resources :buyer_profiles, only: ["new","create"]
 
-#  resources :producer_profiles, only: ["new","create"]
+  resources :producer_profiles, only: ["new","create"]
 
-#  resources :products
+  resources :products
 
-#  devise_for :users, path_prefix: "d", path_names: { sign_in: 'login', sign_up: 'register' }, 
-#    controllers: {confirmations: 'confirmations', sessions: 'sessions', registrations: 'registrations'}
+  devise_for :users, path_prefix: "d", path_names: { sign_in: 'login', sign_up: 'register' }, 
+    controllers: {confirmations: 'confirmations', sessions: 'sessions', registrations: 'registrations'}
 
-#  devise_scope :user do
-#    put '/confirm' => 'confirmations#confirm', as: :user_confirm
-#    get '/login', :to => "devise/sessions#new"
-#    get '/register', :to => "registrations#new"
-#  end
+  devise_scope :user do
+    put '/confirm' => 'confirmations#confirm', as: :user_confirm
+    get '/login', :to => "devise/sessions#new"
+    get '/register', :to => "registrations#new"
+  end
 
-#  resources :user
+  resources :user
 
   # Temporary model to collect email addresses on BETA page
   resources :leads, only: "create"
 
   # Reserved for admin to test different user views
-#  match 'become' => "users#become"
+  match 'become' => "users#become"
 
   root :to => 'public#index'
 
   # Public pages
-#  match "faq" => 'public#faq'
-#  match "about" => 'public#about'
-#  match "contact" => 'public#contact'
-#  match "test" => 'public#test'
+  match "faq" => 'public#faq'
+  match "about" => 'public#about'
+  match "contact" => 'public#contact'
+  match "test" => 'public#test'
 
 end
