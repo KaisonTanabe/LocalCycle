10629ca1b05b1f753b18562eacdb5a138a683e81
Macklin Chaffee <macklin@goldenorbventure.com>
2013-04-23 09:11:02 -0600

    Updated relative image root for seeding

---
+++ lib/tasks/tasks.rb
@@ -0,0 +1,23 @@
+desc "EMAILING list of ineligible students to admins"
+task :ineligible_student_list => :environment do
+  
+  Product.all.each do |p|
+    begin
+      p.pic = File.open("app/assets/images/product_pics/#{p.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.jpg")
+      p.save!
+      puts "Found Product: app/assets/images/product_pics/#{p.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.jpg"
+    rescue 
+      next
+    end
+  end
+  
+  Category.roots.each do |c|
+    begin
+      c.pic = File.open("app/assets/images/category_pics/#{c.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.jpg")
+      c.save!
+      puts "Found category: app/assets/images/category_pics/#{c.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.jpg"
+    rescue
+      next
+    end
+  end
+end
