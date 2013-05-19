602c68b1f66c4008e4681231252adad24fc99126
Macklin Chaffee <macklin@goldenorbventure.com>
2013-05-18 18:05:18 -0600

    Updated LocalCycle 2.0

--- app/models/preferred_buyer_agreement.rb
+++
@@ -1,9 +0,0 @@
-class PreferredBuyerAgreement < ActiveRecord::Base
-  # attr_accessible :title, :body
-
-  belongs_to :agreement
-  belongs_to :buyer_profile
-
-  attr_accessible :agreement_id, :buyer_profile_id
-
-end
--- app/models/preferred_producer_agreement.rb
+++
@@ -1,8 +0,0 @@
-class PreferredProducerAgreement < ActiveRecord::Base
-  # attr_accessible :title, :body
-
-  belongs_to :agreement
-  belongs_to :producer_profile
-
-  attr_accessible :agreement_id, :producer_profile_id
-end
