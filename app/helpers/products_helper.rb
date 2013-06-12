bc94b2634ad22da72ebab415556ec60fe87c8b27
Macklin Chaffee <macklin@goldenorbventure.com>
2013-06-12 13:01:45 -0600

    Updated product view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,6 +3,9 @@ module ProductsHelper
     new_params = {}
     new_params[:name]=params[:name] unless params[:name].blank?
     new_params[:category]=params[:category] unless params[:category].blank?
+    new_params[:selling_units]=params[:selling_units] unless params[:selling_units].blank?
+    new_params[:start_date]=params[:start_date] unless params[:start_date].blank?
+    new_params[:end_date]=params[:end_date] unless params[:end_date].blank?
 
     new_params[:per_page]=params[:per_page] unless params[:per_page].blank?
     new_params
@@ -12,6 +15,9 @@ module ProductsHelper
     new_params = {}
     new_params[:name]=params[:name] unless params[:name].blank?
     new_params[:category]=params[:category] unless params[:category].blank?
+    new_params[:selling_units]=params[:selling_units] unless params[:selling_units].blank?
+    new_params[:start_date]=params[:start_date] unless params[:start_date].blank?
+    new_params[:end_date]=params[:end_date] unless params[:end_date].blank?
 
     new_params[:sort]=params[:sort] unless params[:sort].blank?
     new_params[:direction]=params[:direction] unless params[:direction].blank?
