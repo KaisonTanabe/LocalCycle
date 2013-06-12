0f9193a9ca9b4d5cc3d7da636fb9555b6ae8b610
Macklin Chaffee <macklin@goldenorbventure.com>
2013-06-12 13:22:28 -0600

    Fixed product sorting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -130,7 +130,7 @@ class ProductsController < ApplicationController
   
   def sort_column
     sort = params[:sort] || ''
-    (Product.column_names.include?(sort) || sort == "selling_units.name") ? sort : "products.name"
+    (Product.column_names.include?(sort) || sort == "selling_units.name" || sort == "categories.name") ? sort : "products.name"
   end
 
   def sort_direction
