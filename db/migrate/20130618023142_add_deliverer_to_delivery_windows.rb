7e299d1ae3de602a569f914572fa0977320fa011
David A. Fritz <dafritz@gmail.com>
2013-09-10 12:58:10 -0600

    mod migrations for prod

--- db/migrate/20130618023142_add_deliverer_to_delivery_windows.rb
+++ db/migrate/20130618023142_add_deliverer_to_delivery_windows.rb
@@ -3,6 +3,6 @@ class AddDelivererToDeliveryWindows < ActiveRecord::Migration
     add_column :delivery_windows, :transport_by, :string
     remove_column :agreement_changes, :transport_by
     remove_column :agreements, :transport_by
-    remove_column :users, :transport_by
+    #remove_column :users, :transport_by
   end
 end
