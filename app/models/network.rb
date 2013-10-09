be323fe6237fc89d781f84b6b1fa395915b6cf0f
David A. Fritz <dafritz@gmail.com>
2013-10-08 18:24:27 -0600

    update super user to take on new networks

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,8 +1,19 @@
 class Network < ActiveRecord::Base
+  after_create :associate_with_superadmin
   attr_accessible :name
   has_many :user_networks
   has_many :markets
   
   has_many :users, :through => :user_networks
+  
+  def associate_with_superadmin
+    User.where(:role => 'admin').each do |u|
+      u.networks << self
+      s = u.user_networks.where(:network_id => self.id).first
+      s.approved = true
+      s.save
+    end
+    
+  end
   
 end
